9e716aaeb19f2f916fc78de225b643b616ebc52b
Gastón Caruso <gstn.caruso@gmail.com>
2026-09-24 23:08:19 -0300

    fix(release): tag the commit that produced the assets

--- .template/RendererTest.java
+++ .template/RendererTest.java
@@ -30,6 +30,7 @@ public class RendererTest {
         require(workflow.contains("gh release create"));
         require(workflow.contains("sample-app-app-0.1.${{ github.run_number }}.jar"));
         require(workflow.contains("sample-app_0.1.${{ github.run_number }}_amd64.deb"));
+        require(workflow.contains("--target ${{ github.sha }}"));
     }
 
     private static void rendersEveryFlavor() throws Exception {
